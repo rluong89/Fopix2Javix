dbe258ad4a0a6310ad35d4a753c669213a9720ba
HAOUZANE Yassine <haouzane@informatique.univ-paris-diderot.fr>
2021-02-15 16:24:50 +0100

    Brouillon if, let

--- src/main/scala/trac/transl/Fopix2Anfix.scala
+++ src/main/scala/trac/transl/Fopix2Anfix.scala
@@ -27,17 +27,36 @@ object Fopix2Anfix {
       case S.Def(fid, args, e) :: tl => List()
     }
   }
+  var _count = 0
+
+  def generateLabel(): String = {
+    val new_label = "x" + _count
+    _count += 1
+    new_label
+  }
 
   def trans_expr(e: S.Expr): T.Expr = {
     e match {
-      case Num(n)          => T.Simple(T.Num(n))
-      case Str(s)          => T.Simple(T.Str(s))
-      case Fun(fid)        => T.Simple(T.Fun(fid))
-      case Var(id)         => T.Simple(T.Var(id))
-      case Let(id, e1, e2) => T.Simple(T.Num(0)) /* Yassine */
-      case If(e1, e2, e3)  => T.Simple(T.Num(0)) /* Yassine */
-      case Op(o, e1, e2)   => T.Simple(T.Num(0)) /* Richard */
-      case Prim(p, args)   => T.Simple(T.Num(0)) /* Richard */
+      case Num(n)   => T.Simple(T.Num(n))
+      case Str(s)   => T.Simple(T.Str(s))
+      case Fun(fid) => T.Simple(T.Fun(fid))
+      case Var(id)  => T.Simple(T.Var(id))
+      case Let(id, e1, e2) =>
+        T.Let(id, trans_expr(e1), trans_expr(e2)) /* Yassine */
+      case If(e1, e2, e3) =>
+        val new_id = generateLabel()
+        T.Let(
+          new_id,
+          trans_expr(e1),
+          T.If(
+            /* Pas sur ptet 1 */
+            (BinOp.toCmp(BinOp.Eq), T.Var(new_id), T.Num(0)),
+            trans_expr(e2),
+            trans_expr(e3)
+          )
+        ) /* Yassine */
+      case Op(o, e1, e2) => T.Simple(T.Num(0)) /* Richard */
+      case Prim(p, args) => T.Simple(T.Num(0)) /* Richard */
       case Call(f, args) =>
         T.Simple(T.Num(0)) /* Yassine indirect Richard direct */
     }
