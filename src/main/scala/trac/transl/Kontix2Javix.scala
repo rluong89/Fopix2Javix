16e7eb74eff873c565d382ed8a8ab6ade3e10ee3
HAOUZANE Yassine <haouzane@informatique.univ-paris-diderot.fr>
2021-03-15 00:12:29 +0100

    Copie colle code primitives + cas simple de basicexpr

--- src/main/scala/trac/transl/Kontix2Javix.scala
+++ src/main/scala/trac/transl/Kontix2Javix.scala
@@ -107,10 +107,12 @@ object Kontix2Javix {
   ): List[T.Instruction] = {
     e match {
       /* Yassine */
-      case S.Num(n)   => List()
-      case S.Str(s)   => List()
-      case S.Fun(fid) => List()
-      case S.Var(id)  => List()
+      case S.Num(n) => List(T.Push(n), T.Box)
+      case S.Str(s) => List(T.Ldc(s))
+      case S.Fun(fid) =>
+        val fun_index = funEnv(fid)
+        List(T.Push(fun_index), T.Box)
+      case S.Var(id) => List(T.ALoad(env(id)))
 
       /* Richard */
       case S.BLet(id, e1, e2) => List()
@@ -121,7 +123,72 @@ object Kontix2Javix {
       case S.Op(o, e1, e2) => List()
 
       /* Yassine */
-      case S.Prim(p, args) => List()
+      case S.Prim(p, args) => handlePrim(p, args, funEnv, env)
+    }
+  }
+
+  def generate_dup(n: Integer): List[T.Instruction] = {
+    def aux(n: Integer, acc: List[T.Instruction]): List[T.Instruction] = {
+      if (n == 0)
+        acc
+      else
+        aux(n - 1, T.Dup :: acc)
+    }
+    aux(n, List());
+  }
+
+  def handlePrim(
+      p: T,
+      args: List[S.BasicExpr],
+      funEnv: FunEnv,
+      env: Env
+  ): List[T.Instruction] = {
+    (p, args) match {
+      case (New, List(e1)) =>
+        compile_basic_expr(e1, funEnv, env) ++ List(T.Unbox, T.ANewarray)
+      case (Get, List(e1, e2)) =>
+        compile_basic_expr(e1, funEnv, env) ++ List(T.Checkarray) ++
+          compile_basic_expr(e2, funEnv, env) ++ List(T.Unbox, T.AALoad)
+      case (Set, List(e1, e2, e3)) =>
+        compile_basic_expr(e1, funEnv, env) ++ compile_basic_expr(
+          e2,
+          funEnv,
+          env
+        ) ++ List(T.Unbox) ++
+          compile_basic_expr(e3, funEnv, env) ++ List(
+            T.AAStore,
+            T.Push(0),
+            T.Box
+          )
+      case (Tuple, list) =>
+        val count = list.length
+
+        val l = list.foldLeft((List[T.Instruction](), 0)) { (acc, elt) =>
+          (
+            acc._1 ++ List(T.Push(acc._2)) ++ (compile_basic_expr(
+              elt,
+              funEnv,
+              env
+            )) ++ List(T.AAStore),
+            acc._2 + 1
+          )
+        }
+        List(T.Push(count), T.ANewarray) ++ generate_dup(count) ++ l._1
+      case (Printint, List(e1)) =>
+        compile_basic_expr(e1, funEnv, env) ++ List(
+          T.Unbox,
+          T.IPrint,
+          T.Push(0),
+          T.Box
+        )
+      case (Printstr, List(e1)) =>
+        compile_basic_expr(e1, funEnv, env) ++ List(T.SPrint, T.Push(0), T.Box)
+      case (Cat, List(e1, e2)) =>
+        compile_basic_expr(e1, funEnv, env) ++ compile_basic_expr(
+          e2,
+          funEnv,
+          env
+        ) ++ List(T.SCat)
     }
   }
 
